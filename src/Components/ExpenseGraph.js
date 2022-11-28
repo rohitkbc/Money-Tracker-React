67d14a2048a2587baf86ab1bc175792f2453b2e6
Rohit K Chaudhari <rohitkbc@gmail.com>
2022-11-28 19:24:22 +0530

    charts data dynamically updated

--- src/Components/ExpenseGraph.js
+++ src/Components/ExpenseGraph.js
@@ -4,21 +4,10 @@ import { useEffect, useState } from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function IncomeGraph(props) {
-  const [mydataset, setmyDataset] = useState([]);
-
-  useEffect(() => {
-    props.data.forEach(item => {
-      if (item.type === "Expense") {
-        console.log("Expense: "+ item.amount);
-      }
-    });
-  }, [props.data]);
-
-  var incomeData = [5000, 1000, 2000, 3000, 2000];
-  const data = {
+  const [mydataset, setmyDataset] = useState({
     datasets: [
       {
-        data: incomeData,
+        data: [],
         backgroundColor: [
           "rgba(233, 116, 81, 0.5)",
           "rgba(210, 43, 43, 0.5)",
@@ -36,14 +25,46 @@ function IncomeGraph(props) {
         borderWidth: 1,
       },
     ],
-  };
+  });
+
+  useEffect(() => {
+    var amountArr = [];
+    props.data.forEach(item => {
+      if (item.type === "Expense") {
+        amountArr.push(item.amount);
+      }
+    });
+    
+    setmyDataset({
+      datasets: [
+        {
+          data: amountArr,
+          backgroundColor: [
+            "rgba(233, 116, 81, 0.5)",
+            "rgba(210, 43, 43, 0.5)",
+            "rgba(248, 131, 121, 0.5)",
+            "rgba(250, 160, 160, 0.5)",
+            "rgba(227, 115, 94, 0.5)",
+          ],
+          borderColor: [
+            "rgba(233, 116, 81, 1)",
+            "rgba(210, 43, 43, 1)",
+            "rgba(248, 131, 121, 1)",
+            "rgba(250, 160, 160, 1)",
+            "rgba(227, 115, 94, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    })
+  }, [props.data]);
 
   return (
     <div className="card text-center">
       <div className="card-body">
         <h5 className="card-title">Expense Chart</h5>
         <p className="card-text text-success" style={{ fontWeight: "bold" }}>
-          <Doughnut data={data} style={{ width: "auto" }} />
+          <Doughnut data={mydataset} style={{ width: "auto" }} />
         </p>
       </div>
     </div>
--- src/Components/IncomeGraph.js
+++ src/Components/IncomeGraph.js
@@ -4,20 +4,10 @@ import { useEffect, useState } from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function IncomeGraph(props) {
-  const [mydataset, setmyDataset] = useState([]);
-
-  useEffect(() => {
-    props.data.forEach(item => {
-      if (item.type === "Income") {
-        console.log("Income: "+ item.amount);
-      }
-    });
-  }, [props.data]);
-
-  const data = {
+  const [mydataset, setmyDataset] = useState({
     datasets: [
       {
-        data: [10,20,30,40],
+        data: [],
         backgroundColor: [
           "rgba(11, 218, 81, 0.5)",
           "rgba(34, 139, 34, 0.5)",
@@ -35,14 +25,46 @@ function IncomeGraph(props) {
         borderWidth: 1,
       },
     ],
-  };
-  
+  });
+
+  useEffect(() => {
+    var amountArr = [];
+    props.data.forEach(item => {
+      if (item.type === "Income") {
+        amountArr.push(item.amount);
+      }
+    });
+
+    setmyDataset({
+      datasets: [
+        {
+          data: amountArr,
+          backgroundColor: [
+            "rgba(11, 218, 81, 0.5)",
+            "rgba(34, 139, 34, 0.5)",
+            "rgba(152, 251, 152, 0.5)",
+            "rgba(76, 187, 23, 0.5)",
+            "rgba(50, 205, 50, 0.5)",
+          ],
+          borderColor: [
+            "rgba(11, 218, 81, 1)",
+            "rgba(34, 139, 34, 1)",
+            "rgba(152, 251, 152, 1)",
+            "rgba(76, 187, 23, 1)",
+            "rgba(50, 205, 50, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    })
+  }, [props.data]);
+
   return (
     <div className="card text-center" >
       <div className="card-body">
         <h5 className="card-title">Income Chart</h5>
         <p className="card-text text-success" style={{ fontWeight: "bold" }}>
-          <Doughnut data={data} style={{ width: "auto"}}/>
+          <Doughnut data={mydataset} style={{ width: "auto"}}/>
         </p>
       </div>
     </div>
